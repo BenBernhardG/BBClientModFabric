15e746dbe241faebcb27424161431e425c04b85c
BuildTools <unconfigured@null.spigotmc.org>
2023-08-11 15:22:02 +0200

    Initial commit

---
+++ src/main/java/net/bernhardbmx/bbclient/BBClient.java
@@ -0,0 +1,13 @@
+package net.bernhardbmx.bbclient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BBClient implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
